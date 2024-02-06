86b706f4aae45c6a784be8c80296136b219790f1
gsilva <gabriel.l.silva@intel.com>
2024-02-06 09:09:39 -0700

    add llava and better prompt examples

--- run-demo.py
+++ run-demo.py
@@ -6,10 +6,10 @@ import json
 from transformers import AutoConfig, AutoTokenizer
 from transformers.generation import GenerationConfig
 from transformers.utils import check_min_version
-from intel_extension_for_transformers.transformers import WeightOnlyQuantConfig #, AutoModelForCausalLM
-from intel_extension_for_transformers.transformers.modeling import AutoModelForCausalLM
-from intel_extension_for_transformers.llm.quantization.utils import convert_dtype_str2torch
+from intel_extension_for_transformers.transformers import WeightOnlyQuantConfig
 import intel_extension_for_pytorch as ipex
+from PIL import Image
+import requests
 
 torch.manual_seed(1234)
 
@@ -17,7 +17,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--use_woq_4bit", default=False, action="store_true")
 parser.add_argument("--use_gptq", default=False, action="store_true")
 parser.add_argument("--device_map", nargs="?", default="cpu")
-parser.add_argument("--model_name", nargs="?", default="Qwen/Qwen-7B")
+parser.add_argument("--model_name", nargs="?", default="Qwen/Qwen-7B-Chat")
 parser.add_argument("--save_dir", nargs="?", default="")
 
 args = parser.parse_args()
@@ -34,8 +34,13 @@ tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 if device_map == "cpu" and not load4bit:
   # use cpu only
   print("Loading FP32 model for CPU")
-  from intel_extension_for_transformers.transformers import AutoModelForCausalLM
-  model = AutoModelForCausalLM.from_pretrained(model_name, device_map="cpu", trust_remote_code=True).eval()
+  if "llava-1.5" in model_name:
+    from transformers import  AutoProcessor, LlavaForConditionalGeneration
+    model = LlavaForConditionalGeneration.from_pretrained(model_name, trust_remote_code=True).eval()
+    processor = AutoProcessor.from_pretrained(model_name)
+  else:
+    from intel_extension_for_transformers.transformers import AutoModelForCausalLM
+    model = AutoModelForCausalLM.from_pretrained(model_name, device_map=device_map, trust_remote_code=True).eval()
 
 elif device_map == "xpu" and load4bit == False:
   # Intel Arc/Flex GPU/XPU device
@@ -109,33 +114,54 @@ print("Optimized model: ", model)
 print("Starting 1st inference. This will take a while to initialize.....")
 
 start_time = time.time()
-query = "Why are LLMs large is size?"
-if model_name == "Qwen/Qwen-VL":
+query = "USER: Why are LLMs large is size?\nASSISTANT:"
+image_url = 'https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg'
+if model_name == "Qwen/Qwen-VL-Chat":
   query = tokenizer.from_list_format([
-    {'image': 'https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg'},
+    {'image': image_url},
     {'text': 'Describe this picture.'},
   ])
+  input_ids = tokenizer(query, return_tensors="pt").input_ids.to(args.device_map)
+elif "llava-1.5" in model_name:
+  raw_image = Image.open(requests.get(image_url, stream=True).raw)
+  query = "<image>\nUSER: Describe this picture.\nASSISTANT:"
+  input_ids = processor(text=query, images=raw_image, return_tensors="pt")
+else:
+  input_ids = tokenizer(query, return_tensors="pt").input_ids.to(args.device_map)
 
-input_ids = tokenizer(query, return_tensors="pt").input_ids.to(args.device_map)
-response = model.generate(input_ids)
-response = tokenizer.batch_decode(response, skip_special_tokens=True)
+if "llava-1.5" in model_name:
+  response = model.generate(**input_ids, max_length=5048)
+  response = processor.batch_decode(response, skip_special_tokens=True)
+else:
+  response = model.generate(input_ids, max_length=5048)
+  response = tokenizer.batch_decode(response, skip_special_tokens=True)
+
 if device_map == "xpu":
   torch.xpu.synchronize()
 elapsed_time = time.time() - start_time
 print("First inference response: " , response)
 print("Seconds taken for first query: ", elapsed_time)
 
-print("Starting inference second query...")
+print("Starting inference for second query...")
 start_time = time.time()
-query = "Explain the pros and cons of AI with Intel architecture."
-if model_name == "Qwen/Qwen-VL":
+query = "USER: Explain the pros and cons of AI with Intel architecture.\nASSISTANT:"
+if model_name == "Qwen/Qwen-VL-Chat":
   query = tokenizer.from_list_format([
     {'text': 'Explain the pros and cons of AI with Intel architecture.'}
   ])
+  input_ids = tokenizer(query, return_tensors="pt").input_ids.to(args.device_map)
+elif "llava-1.5" in model_name:
+  input_ids = processor(query, None, return_tensors="pt")
+else:
+  input_ids = tokenizer(query, return_tensors="pt").input_ids.to(args.device_map)
 
-input_ids = tokenizer(query, return_tensors="pt").input_ids.to(args.device_map)
-response = model.generate(input_ids)
-response = tokenizer.batch_decode(response, skip_special_tokens=True)
+if "llava-1.5" in model_name:
+  response = model.generate(**input_ids, max_length=5048)
+  response = processor.batch_decode(response, skip_special_tokens=True)
+else:
+  response = model.generate(input_ids, max_length=5048)
+  response = tokenizer.batch_decode(response, skip_special_tokens=True)
+
 elapsed_time = time.time() - start_time
 print("Second inference response: " , response)
 print("Seconds taken for second query: ", elapsed_time)
